37a9f149be018c4a2b43ddfbcfd6de7270ab3d82
Alex <trainofbird@gmail.com>
2023-06-03 15:51:11 +0300

    Все задачи решены

---
+++ Ex41/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Main();
+
+void Main()
+{
+    int countNum = Prompt("Введите запланированное Вами количество обрабатываемых программой чисел: ");
+    int[] array = GetArrayManual(countNum);
+    CheckMoreZeroNumInArray(array);
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine()!);
+    return value;
+}
+
+int[] GetArrayManual(int size)
+{
+    int[] userArray = new int[size];
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-е число: ");
+        userArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return userArray;
+}
+
+void CheckMoreZeroNumInArray(int[] checkedArray)
+{
+    int count = 0;
+    for (int j = 0; j < checkedArray.Length; j++)
+    {
+        if (checkedArray[j] < 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество введённых Вами отрицательных чисел (меньше 0) равняется {count} ");
+}
---
+++ Ex43/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения
+// двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Main();
+
+void Main()
+{
+    int b1 = Prompt("Введите число b1: ");
+    int k1 = Prompt("Введите число k1: ");
+    int b2 = Prompt("Введите число b2: ");
+    int k2 = Prompt("Введите число k2: ");
+    if (CheckValideNums(k1, k2) == true)
+    {
+        FindPoint(b1, k1, b2, k2);
+    }
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine()!);
+    return value;
+}
+
+bool CheckValideNums(int kFirst, int kSecond)
+{
+    if (kFirst - kSecond == 0)
+    {
+        Main();
+        return false;
+    }
+    return true;
+}
+
+void FindPoint(int bOne, int kOne, int bTwo, int kTwo)
+{
+    double bI = Convert.ToDouble(bOne);
+    double kI = Convert.ToDouble(kOne);
+    double bII = Convert.ToDouble(bTwo);
+    double kII = Convert.ToDouble(kTwo);
+    double x = (bII - bI) / (kI - kII);
+    double y = kI * x + bI;
+    Console.WriteLine($"Координаты точки пересечения прямых: ({x}, {y})");
+}
